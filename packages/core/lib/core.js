366ceb03655ab120da4066724c2498e5d6202038
izhaong <izhaong@outlook.com>
2023-01-19 22:26:05 +0800

    test: lerna changed

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,7 +1,16 @@
+/*
+ * @Author: 仲灏<izhaong@outlook.com>🌶🌶🌶
+ * @Date: 2023-01-17 18:00:30
+ * @LastEditTime: 2023-01-19 22:25:37
+ * @LastEditors: 仲灏<izhaong@outlook.com>🌶🌶🌶
+ * @Description:  
+ * @FilePath: /zh-cli/packages/core/lib/core.js
+ */
 'use strict';
 
 module.exports = core;
 
 function core() {
-    return "Hello from core";
+    return "Hello from core11";
 }
+
